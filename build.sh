b6f697d609088f0a9d16f83c69a79c44b985a682
ims <optimizes@126.com>
2020-10-05 09:34:56 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -4,20 +4,23 @@
 ##########################################################
 #!/bin/bash
 
-set -x
-
-SOURCE_DIR=`pwd`/cmake-submodule
-BUILD_DIR=${BUILD_DIR:-./build}
+set -x -e
+CUR_DIR=$(cd `dirname $0`;pwd)
+SOURCE_DIR=$CUR_DIR/cmake-submodule
+BUILD_DIR=${BUILD_DIR:-$CUR_DIR/build}
+OUTDIR_DIR=$CUR_DIR/output
 BUILD_TYPE=${BUILD_TYPE:-debug}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
 CXX=${CXX:-g++}
-
+set +x
 ln -sf $BUILD_DIR/$BUILD_TYPE-cpp11/compile_commands.json
 
-mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
+rm -rf $OUTDIR_DIR
+mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 $OUTDIR_DIR\
   && cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+           -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$OUTDIR_DIR \
            $SOURCE_DIR \
   && make $*
